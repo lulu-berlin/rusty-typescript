8fc2f07bacf926b2f916280a272708d1335c6a81
Ya'ar Hever <yaarhever@gmail.com>
2019-04-17 11:19:19 +0200

    Bundle the JS output with webpack  (#6)

--- build.ts
+++ build.ts
@@ -4,23 +4,30 @@ import {readFileSync} from 'fs';
 import {dest, series, src} from 'gulp';
 import replace = require('gulp-replace');
 import path = require('path');
+import webpack = require('webpack');
 
 const WASM_PACK_DEST = 'wasm';
 const PROJ_NAME = 'rusty-typescript';
 const TMP = 'tmp';
 const DIST = 'dist';
-const BG_JS_FILENAME = `${PROJ_NAME.replace('-', '_')}_bg.js`;
-const BG_WASM_FILENAME = `${PROJ_NAME.replace('-', '_')}_bg.wasm`;
+const SNAKECASE_PROJ_NAME = PROJ_NAME.replace('-', '_');
+const JS_FILENAME =  `${SNAKECASE_PROJ_NAME}.js`;
+const BG_JS_FILENAME = `${SNAKECASE_PROJ_NAME}_bg.js`;
+const BG_WASM_FILENAME = `${SNAKECASE_PROJ_NAME}_bg.wasm`;
 
 function cleanWasmPackDest() {
-  return del(WASM_PACK_DEST);
+  return del(path.resolve(__dirname, WASM_PACK_DEST));
 }
 
 function cleanTmp() {
-  return del(TMP);
+  return del(path.resolve(__dirname, TMP));
 };
 
-export const clean = series(cleanWasmPackDest, cleanTmp);
+function cleanDist() {
+  return del(path.resolve(__dirname, DIST));
+}
+
+export const clean = series(cleanWasmPackDest, cleanTmp, cleanDist);
 
 const execCommand = (command: string) => new Promise(
   (resolve, reject) => {
@@ -53,7 +60,7 @@ async function wasmPack() {
  * serialized as base64 and copy it into the tmp/ folder.
  */
 function inlineWasm() {
-  const wasm = readFileSync(path.resolve(WASM_PACK_DEST, BG_WASM_FILENAME));
+  const wasm = readFileSync(path.resolve(__dirname, WASM_PACK_DEST, BG_WASM_FILENAME));
   const base64 = wasm.toString('base64');
   const bytesLines = [
     `const atob = typeof window !== 'undefined' ? window.atob : b64 => Buffer.from(b64, 'base64').toString('binary');`,
@@ -64,7 +71,7 @@ function inlineWasm() {
     `}\n`
   ].join('\n');
 
-  return src([path.resolve(WASM_PACK_DEST, BG_JS_FILENAME)])
+  return src([path.resolve(__dirname, WASM_PACK_DEST, BG_JS_FILENAME)])
     // remove the import of 'path'
     .pipe(replace(/const path = .*\n/g, ''))
     // replace the loading of the wasm bytes to use the inlined base64 version
@@ -72,13 +79,56 @@ function inlineWasm() {
     .pipe(dest(TMP));
 }
 
+/**
+ *
+ */
+function patchJsFile() {
+  return src([path.resolve(__dirname, WASM_PACK_DEST, JS_FILENAME)])
+    .pipe(replace(/const TextDecoder = .*\n/g, `require('fast-text-encoding');\n`))
+    .pipe(dest(TMP));
+}
+
 function copyToTmp() {
   return src([
-    path.resolve(WASM_PACK_DEST, '**'),
-    `!${path.resolve(WASM_PACK_DEST, BG_WASM_FILENAME)}`,
-    `!${path.resolve(WASM_PACK_DEST, BG_JS_FILENAME)}`
+    path.resolve(__dirname, WASM_PACK_DEST, '**'),
+    `!${path.resolve(__dirname, WASM_PACK_DEST, BG_WASM_FILENAME)}`,
+    `!${path.resolve(__dirname, WASM_PACK_DEST, BG_JS_FILENAME)}`,
+    `!${path.resolve(__dirname, WASM_PACK_DEST, JS_FILENAME)}`
   ])
     .pipe(dest(TMP));
 }
 
-export const build = series(installWasmPack, clean, wasmPack, inlineWasm, copyToTmp);
+function bundle() {
+  const config: webpack.Configuration = {
+    entry: `./${JS_FILENAME}`,
+    output: {
+      filename: './wasm.js',
+      path: path.resolve(__dirname, DIST),
+      library: 'RustyTypeScript',
+      libraryTarget: 'var'
+    },
+    context: path.resolve(__dirname, TMP)
+  };
+
+  return new Promise((resolve, reject) => webpack(
+    config,
+    (err) => {
+      if (err) {
+        console.error(err);
+        reject(err);
+      } else {
+        resolve();
+      }
+    }
+  ));
+}
+
+export const build = series(
+  installWasmPack,
+  clean,
+  wasmPack,
+  inlineWasm,
+  patchJsFile,
+  copyToTmp,
+  bundle
+);
